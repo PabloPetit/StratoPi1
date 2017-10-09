6d5b08d582626bb6939711e10c0c6132af1e3ba2
PabloPetit <pablopetit94@gmail.com>
2017-10-09 20:55:46 +0200

    test

--- Python/MissionControl/testZone.py
+++ Python/MissionControl/testZone.py
@@ -9,6 +9,7 @@ from camera_module import *
 from adc_module import *
 from gsm_module import *
 
+print("StartUp")
 i = 0
 while os.path.isdir(HOME_PATH):
     while HOME_PATH[len(HOME_PATH)].isdigit() or HOME_PATH[len(HOME_PATH)] == "/":
@@ -16,6 +17,7 @@ while os.path.isdir(HOME_PATH):
     HOME_PATH += str(i) + "/"
     i+=1
 
+print("Done")
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
